b83df7d3e76689a41ee3057f90348e28512ab813
jakoch <jakoch@web.de>
2016-09-20 19:26:36 +0200

    added version detection

--- src/CMake/Modules/FindFife.cmake
+++ src/CMake/Modules/FindFife.cmake
@@ -12,6 +12,21 @@ endif(FIFE_INCLUDE_DIR)
 
 find_path(FIFE_INCLUDE_DIR fife/core/version.h)
 
+# append "fife/core" to "include" path
+set(FIFE_INCLUDE_DIR ${FIFE_INCLUDE_DIR}/fife/core)
+
+#------------------------------------------------------------------------------
+#                           Detect Version
+#------------------------------------------------------------------------------
+
+if(FIFE_INCLUDE_DIR AND EXISTS "${FIFE_INCLUDE_DIR}/version.h")
+   file(STRINGS "${FIFE_INCLUDE_DIR}/version.h" FIFE_VERSION_LINE REGEX "^(.+)([0-9]+)\\.([0-9]+)\\.([0-9]+)\"$")
+   string(REGEX REPLACE "^[ \t]#define FIFE_VERSION \"([0-9]+)\\.([0-9]+)\\.([0-9]+)\"$" "\\1.\\2.\\3" FIFE_VERSION "${FIFE_VERSION_LINE}")
+   set(FIFE_VERSION_STRING ${FIFE_VERSION})
+   unset(FIFE_VERSION_LINE)
+   unset(FIFE_VERSION)
+endif()
+
 # MSVC built fife may be named fife_static.
 # The provided project files name the library with the lib prefix.
 find_library(FIFE_LIBRARY NAMES fife fife_static libfife libfife_static)
@@ -19,7 +34,11 @@ find_library(FIFE_LIBRARY NAMES fife fife_static libfife libfife_static)
 # Handle the QUIETLY and REQUIRED arguments and set FIFE_FOUND
 # to TRUE if all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FIFE DEFAULT_MSG FIFE_LIBRARY FIFE_INCLUDE_DIR)
+
+find_package_handle_standard_args(FIFE 
+    REQUIRED_VARS FIFE_LIBRARY FIFE_INCLUDE_DIR
+    VERSION_VAR   FIFE_VERSION_STRING
+)
 
 if(FIFE_FOUND)
     set(FIFE_LIBRARIES ${FIFE_LIBRARY})
