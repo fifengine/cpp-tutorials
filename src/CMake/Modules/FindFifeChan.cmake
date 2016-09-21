ff73ac75171e06e7222a28189cae0bbcc3d6e6c4
jakoch <jakoch@web.de>
2016-09-21 21:16:43 +0200

    add support for components e.g. find_package(FifeChan COMPONENTS opengl sdl REQUIRED)

--- src/CMake/Modules/FindFifeChan.cmake
+++ src/CMake/Modules/FindFifeChan.cmake
@@ -10,22 +10,59 @@ if(FIFECHAN_INCLUDE_DIR)
     set(FIFECHAN_FIND_QUIETLY TRUE)
 endif(FIFECHAN_INCLUDE_DIR)
 
+#------------------------------------------------------------------------------
+#                       Find FIFECHAN_INCLUDE_DIR                                
+#------------------------------------------------------------------------------
+
 find_path(FIFECHAN_INCLUDE_DIR fifechan/fifechan.hpp)
 
 # append "fifechan" to "include" path
 set(FIFECHAN_INCLUDE_DIR ${FIFECHAN_INCLUDE_DIR}/fifechan)
 
-# MSVC built fifechan may be named fifechan_static.
-# The provided project files name the library with the lib prefix.
+#------------------------------------------------------------------------------
+#                       Find FIFECHAN_LIBRARY                              
+#------------------------------------------------------------------------------
+
+# support alternative library names: suffix "_static" and prefix "lib"
 find_library(FIFECHAN_LIBRARY NAMES fifechan fifechan_static libfifechan libfifechan_static)
 
-# Handle the QUIETLY and REQUIRED arguments and set FIFECHAN_FOUND
-# to TRUE if all listed variables are TRUE.
+#------------------------------------------------------------------------------
+#                           Find libraries (components)
+#
+#   Example: find_package(FifeChan COMPONENTS opengl sdl REQUIRED)                                    
+#------------------------------------------------------------------------------
+
+IF(FifeChan_FIND_COMPONENTS)
+  
+  message(STATUS "Found the following FifeChan libraries:")
+
+  FOREACH(component ${FifeChan_FIND_COMPONENTS})
+
+    find_library(FIFECHAN_${component}_LIBRARY NAMES fifechan_${component})
+
+    if(FIFECHAN_${component}_LIBRARY)        
+        SET(FIFECHAN_${component}_FOUND 1)
+        list(APPEND FIFECHAN_COMPONENTS "${FIFECHAN_${component}_LIBRARY}")
+        message(STATUS "  fifechan_${component}")
+    else()
+        SET(FIFECHAN_${component}_FOUND 0)
+        message(STATUS "  fifechan_${component} not found.")
+    endif()
+
+  ENDFOREACH()
+ENDIF()
+
+# - handle the QUIETLY and REQUIRED arguments
+# - set FIFECHAN_FOUND to TRUE if all REQUIRED_VARS are TRUE
+# - additionally handle components, which are found: FIFECHAN_${component}_FOUND
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FIFECHAN DEFAULT_MSG FIFECHAN_LIBRARY FIFECHAN_INCLUDE_DIR)
+find_package_handle_standard_args(FIFECHAN 
+    REQUIRED_VARS FIFECHAN_LIBRARY FIFECHAN_INCLUDE_DIR 
+    HANDLE_COMPONENTS
+)
 
 if(FIFECHAN_FOUND)
-    set(FIFECHAN_LIBRARIES ${FIFECHAN_LIBRARY})
+    set(FIFECHAN_LIBRARIES ${FIFECHAN_LIBRARY} ${FIFECHAN_COMPONENTS})
 endif(FIFECHAN_FOUND)
 
 mark_as_advanced(FIFECHAN_INCLUDE_DIR FIFECHAN_LIBRARY)
